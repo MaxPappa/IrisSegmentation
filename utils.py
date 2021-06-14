25bd71b31e440fd67d9d00d4c0204fd3ade85d09
Umberto Valleriani <umberto.valleriani@gmail.com>
2021-06-14 12:34:09 +0200

    hopefully vectorize lineInt (+ Black formatting)

--- utils.py
+++ utils.py
@@ -1,23 +1,27 @@
+import numba
 import numpy as np
 import cv2
 
-def boundMask(img: np.ndarray, n: int, center: tuple, ray: int, color: bool, full: bool):
-    #circ = 2*np.pi*ray
-    theta = (2*np.pi) / n # / 2*np.pi*ray
-    mask = np.zeros_like(img[:,:,0], dtype=bool) if color else np.zeros_like(img, dtype=bool)
-    rows,cols = img.shape[:2]
+
+def boundMask(
+    img: np.ndarray, n: int, center: tuple, ray: int, color: bool, full: bool
+):
+    # circ = 2*np.pi*ray
+    theta = (2 * np.pi) / n  # / 2*np.pi*ray
+    mask = (
+        np.zeros_like(img[:, :, 0], dtype=bool)
+        if color
+        else np.zeros_like(img, dtype=bool)
+    )
+    rows, cols = img.shape[:2]
 
     if not full:
-        angle_1 = np.arange(theta, np.pi/4, theta)
-        angle_2 = np.arange((np.pi*3)/4, (np.pi*5)/4, theta)
-        angle_3 = np.arange((np.pi*7)/4, 2*np.pi, theta)
-        angles = np.concatenate([angle_1, angle_2, angle_3])
-    # else:
-    #     angle_1 = np.arange(theta, (np.pi)/4, theta)
-    #     angle_2 = np.arange((np.pi*3)/4, 2*np.pi, theta)
-    #     angles = np.concatenate([angle_1, angle_2])
+        angle_1 = np.arange(theta, np.pi / 4, theta)
+        angle_2 = np.arange((np.pi * 3) / 4, (np.pi * 5) / 4, theta)
+        angle_3 = np.arange((np.pi * 7) / 4, 2 * np.pi, theta)
+        angles = np.concatenate((angle_1, angle_2, angle_3))
     else:
-        angles = np.arange(theta, 2*np.pi, theta)
+        angles = np.arange(theta, 2 * np.pi, theta)
 
     X = np.round(ray * np.cos(angles) + center[0]).astype(int)
     Y = np.round(ray * np.sin(angles) + center[1]).astype(int)
@@ -28,16 +32,27 @@ def boundMask(img: np.ndarray, n: int, center: tuple, ray: int, color: bool, ful
         mask[Y, X] = True
         return mask, np.count_nonzero(mask)
 
-lumaF = lambda img, mask: np.sum(img[mask,0]*0.114) + np.sum(img[mask,1]*0.587) + np.sum(img[mask,2]*0.299)
+
+def lumaF(img: np.ndarray, mask: np.ndarray):
+    return (
+        np.sum(img[mask, 0] * 0.114)
+        + np.sum(img[mask, 1] * 0.587)
+        + np.sum(img[mask, 2] * 0.299)
+    )
+
 
 def lineInt(img: np.ndarray, center: tuple, radii: np.ndarray, color: bool, full: bool):
-    l = list()
     rows, cols = img.shape[:2]
-    for r in radii:
-        mask, numPx = boundMask(img=img, n=600, center=center, ray=r, color=color, full=full)
+    l = np.zeros((len(radii),))
+    for i, r in enumerate(radii):
+        mask, numPx = boundMask(
+            img=img, n=600, center=center, ray=r, color=color, full=full
+        )
         if numPx == 0:
-            l.append(0)
-            return np.array(l), True
-        if color: l.append(lumaF(img,mask) / numPx)
-        else: l.append(np.sum(img[mask]) / numPx)
-    return np.array(l), True
+            return l[: i + 1], True
+        if color:
+
+            l[i] = lumaF(img, mask) / numPx
+        else:
+            l[i] = np.sum(img[mask]) / numPx
+    return l, True
